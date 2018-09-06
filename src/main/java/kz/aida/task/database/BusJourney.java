4a57feeb8ec9b28b6c6ce63f1af810b8a3b3cf28
Radik Sharipov <radik.sharipov24@gmail.com>
2018-09-07 02:02:22 +0600

    BusJourney class (db table: bus_journey)

---
+++ src/main/java/kz/aida/task/database/BusJourney.java
@@ -0,0 +1,136 @@
+package kz.aida.task.database;
+
+public class BusJourney {
+    private int id;
+    private String fromStation;
+    private int fromTime;
+    private String toStation;
+    private int toTime;
+    private int price;
+    private String driver;
+
+    // schedule
+    private boolean isMonday;
+    private boolean isTuesday;
+    private boolean isWednesday;
+    private boolean isThursday;
+    private boolean isFriday;
+    private boolean isSaturday;
+    private boolean isSunday;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFromStation() {
+        return fromStation;
+    }
+
+    public void setFromStation(String fromStation) {
+        this.fromStation = fromStation;
+    }
+
+    public int getFromTime() {
+        return fromTime;
+    }
+
+    public void setFromTime(int fromTime) {
+        this.fromTime = fromTime;
+    }
+
+    public String getToStation() {
+        return toStation;
+    }
+
+    public void setToStation(String toStation) {
+        this.toStation = toStation;
+    }
+
+    public int getToTime() {
+        return toTime;
+    }
+
+    public void setToTime(int toTime) {
+        this.toTime = toTime;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public boolean isMonday() {
+        return isMonday;
+    }
+
+    public void setMonday(boolean monday) {
+        isMonday = monday;
+    }
+
+    public boolean isTuesday() {
+        return isTuesday;
+    }
+
+    public void setTuesday(boolean tuesday) {
+        isTuesday = tuesday;
+    }
+
+    public boolean isWednesday() {
+        return isWednesday;
+    }
+
+    public void setWednesday(boolean wednesday) {
+        isWednesday = wednesday;
+    }
+
+    public boolean isThursday() {
+        return isThursday;
+    }
+
+    public void setThursday(boolean thursday) {
+        isThursday = thursday;
+    }
+
+    public boolean isFriday() {
+        return isFriday;
+    }
+
+    public void setFriday(boolean friday) {
+        isFriday = friday;
+    }
+
+    public boolean isSaturday() {
+        return isSaturday;
+    }
+
+    public void setSaturday(boolean saturday) {
+        isSaturday = saturday;
+    }
+
+    public boolean isSunday() {
+        return isSunday;
+    }
+
+    public void setSunday(boolean sunday) {
+        isSunday = sunday;
+    }
+
+    public int getJourneyTime() {
+        return toTime - fromTime;
+    }
+}
